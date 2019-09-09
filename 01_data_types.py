6fe27a20f32e9dbc0741549e9fb4911ef29f1cbb
Sudhakar17 <sudhakarece17@gmail.com>
2019-09-09 15:15:14 +0200

    int, string and double - sum operation

---
+++ 01_data_types.py
@@ -0,0 +1,16 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+# Read and save an integer, double, and String to your variables.
+i_1 = int(input())
+d_1 = float(input())
+s_1 = input()
+
+# Print the sum of both integer variables on a new line.
+print(i+i_1)
+# Print the sum of the double variables on a new line.
+print(d+d_1)
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print(s+s_1)
