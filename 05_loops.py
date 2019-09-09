10c8af0fad13acf1da77c6d9237e3f7fc91b2ff9
Sudhakar17 <sudhakarece17@gmail.com>
2019-09-09 15:44:54 +0200

    print the 10 multiples of n

---
+++ 05_loops.py
@@ -0,0 +1,11 @@
+"""
+Task
+Given an integer,n , print its first 10 multiples. Each multiple n x i
+(where 1<=i<=10) should be printed on a new line in the form: n x i = result.
+"""
+
+import sys
+n = int(input().strip())
+for i in range(1,11):
+    result = n*i
+    print("{} x {} = {}".format(n,i,result))
