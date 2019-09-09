baf0d51487004092d409d436bb664e1a8d63fde9
Sudhakar17 <sudhakarece17@gmail.com>
2019-09-09 15:38:50 +0200

    calculate the meal cost

---
+++ 02_operators.py
@@ -0,0 +1,19 @@
+"""Given the meal price (base cost of a meal), tip percent (the percentage of the meal price being added as tip),
+    and tax percent (the percentage of the meal price being added as tax) for a meal,
+    find and print the meal's total cost."""
+
+
+import sys
+def calculate_meal_cost():
+    tip = meal_cost * (tip_percent / 100)
+    tax = meal_cost * (tax_percent / 100)
+
+    total_cost = round(meal_cost + tip + tax)
+    print("The total meal cost is {} dollars.".format(total_cost))
+
+
+if __name__ == "__main__":
+    meal_cost = float(input().strip())
+    tip_percent = int(input().strip())
+    tax_percent = int(input().strip())
+    calculate_meal_cost()
