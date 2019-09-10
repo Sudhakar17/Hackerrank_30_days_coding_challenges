fb1c09b0b1894c5bc86fde366b3ffd3d9b0d8756
Sudhakar17 <sudhakarece17@gmail.com>
2019-09-10 10:21:50 +0200

    example for interface

---
+++ 19_interfaces.py
@@ -0,0 +1,27 @@
+class AdvancedArithmetic(object):
+    def divisorSum(self, n):
+        raise NotImplementedError
+
+class Calculator(AdvancedArithmetic):
+    def divisorSum(self, n):
+        import math
+        result = 0
+        i = 1
+        while i<= int(math.sqrt(n)) :
+            # if 'i' is divisor of 'num'
+            if n % i == 0:
+                # if both divisors are same then
+                # add it only once else add both
+                if i == n // i:
+                    result = result + i;
+                else :
+                    result = result + (i + n // i);
+            i = i + 1
+        return result;
+
+
+n = int(input())
+my_calculator = Calculator()
+s = my_calculator.divisorSum(n)
+print("I implemented: " + type(my_calculator).__bases__[0].__name__)
+print(s)
