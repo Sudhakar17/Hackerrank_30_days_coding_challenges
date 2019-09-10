db7bcf16771d7be3308d27446041758174b3d731
Sudhakar17 <sudhakarece17@gmail.com>
2019-09-10 10:11:41 +0200

    scope of the variable

---
+++ 14_scope.py
@@ -0,0 +1,20 @@
+class Difference:
+    def __init__(self, a):
+        self.__elements = a
+
+    # Add your code here
+    maximumDifference = 0
+    def computeDifference(self):
+        self.__elements = sorted(self.__elements)
+        for i in range(len(self.__elements)-1):
+            tmp = abs(self.__elements[0] - self.__elements[i + 1])
+            if  tmp > self.maximumDifference:
+                self.maximumDifference = tmp
+
+_ = input()
+a = [int(e) for e in input().split(' ')]
+
+d = Difference(a)
+d.computeDifference()
+
+print(d.maximumDifference)
