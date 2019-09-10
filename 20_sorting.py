1e2c8d1ca03e51b8a8be17288a19228c5fe9606f
Sudhakar17 <sudhakarece17@gmail.com>
2019-09-10 10:24:16 +0200

    bubble sorting

---
+++ 20_sorting.py
@@ -0,0 +1,20 @@
+# bubble sorting
+
+import sys
+
+n = int(input().strip())
+a = list(map(int, input().strip().split(' ')))
+# Write Your Code Here
+swap_counter = 0
+for i in range(n):
+    
+    for j in range(n-1):
+        if a[j] > a[j+1]:
+            tmp = a[j]
+            a[j] = a[j+1]
+            a[j+1] = tmp
+            swap_counter +=1
+            
+print ("Array is sorted in {} swaps.".format(swap_counter))
+print ("First Element: {}".format(a[0]))
+print ("Last Element: {}".format(a[n-1]))
